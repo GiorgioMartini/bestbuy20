6eba182520ad9b49892bcf088e2ff1945da1b2ff
giorgio <jorgeantonio82@gmail.com>
2025-01-08 22:05:58 +0100

    Implement product ordering and display functionality in main.py
    - Added a new function `show_all_products` to list all products in the store.
    - Enhanced the `start` function to include product ordering capabilities, allowing users to select products and specify quantities.
    - Improved input validation for product selection and quantity input.
    - Streamlined the user interaction flow in the store menu.

--- main.py
+++ main.py
@@ -7,11 +7,50 @@ product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
                ]
 best_buy = Store(product_list)
 
+def show_all_products(store):
+    print("All products in store:")
+    for i, product in enumerate(store.get_all_products(), 1):
+        print(f"{i}. {product.name}")
 
 def start(store):
-    print("Store Menu")
-    print("----------")
-    print("1. List all products in store")
-    print("2. Show total amount in store")
-    print("3. Make an order")
-    print("4. Quit")
+
+
+    while True:
+        print("Store Menu")
+        print("----------")
+        print("1. List all products in store")
+        print("2. Show total amount in store")
+        print("3. Make an order")
+        print("4. Quit")        
+        user_input = input("Enter your choice: ")
+        if user_input == "1":
+            show_all_products(store)
+        elif user_input == "2":
+            print("Total amount in store: ", store.get_total_quantity())
+        elif user_input == "3":
+            show_all_products(store)
+            while True:
+                wantedProduct = input("Which product # do you want? ")
+                if not wantedProduct.isdigit():
+                    print("Product number must be a number")
+                    break
+                wanted_amt = input("What amount do you want? ")
+                if not wanted_amt.isdigit():
+                    print("Amount must be a number") 
+                    break
+                
+                product_idx = int(wantedProduct) - 1  # Subtract 1 because indices start at 0
+                quantity = int(wanted_amt)
+                
+                product = store.get_all_products()[product_idx]
+                
+                shopping_list = [(product, quantity)]
+                total = store.order(shopping_list)
+                print(f"Order made! Total payment: ${total}")
+                break
+        elif user_input == "4":
+            print("Thank you for shopping with us!")
+            break
+
+if __name__ == "__main__":
+    start(best_buy)
