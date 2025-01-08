8eb485e2b4a24f8df1a041794b24d7d7c8e475d8
giorgio <jorgeantonio82@gmail.com>
2025-01-08 20:03:48 +0100

    Enhance main.py with store menu functionality
    - Added a `start` function to display a menu for interacting with the Store.
    - Improved formatting of the product list initialization for better readability.
    - Removed direct calls to store methods from the global scope to streamline the program structure.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from products import Product
 from store import Store
 
-product_list = [Product("MacBook Air M2", price=1450, quantity=100),
-                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                Product("Google Pixel 7", price=500, quantity=250),
+product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
+                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 Product("Google Pixel 7", price=500, quantity=250)
                ]
+best_buy = Store(product_list)
 
-store = Store(product_list)
-products = store.get_all_products()
-print(store.get_total_quantity())
-print(store.order([(products[0], 1), (products[1], 2)]))
+
+def start(store):
+    print("Store Menu")
+    print("----------")
+    print("1. List all products in store")
+    print("2. Show total amount in store")
+    print("3. Make an order")
+    print("4. Quit")
