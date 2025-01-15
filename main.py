8a8352126ac3d48892d891e0964f66e3e5b6a9fe
giorgio <jorgeantonio82@gmail.com>
2025-01-15 21:43:26 +0100

    Refactor product imports and enhance product classes
    - Updated import statements in main.py to use module-level imports for better clarity.
    - Added new product types: NonStockedProduct and LimitedProduct in products.py, with validation and methods for purchasing and displaying product information.
    - Enhanced product_list in main.py to include the new product types, improving the store's inventory management capabilities.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from products import Product
-from store import Store
+import products
+import store
 
-product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
-                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                 Product("Google Pixel 7", price=500, quantity=250)
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250),
+                 products.NonStockedProduct("Windows License", price=125),
+                 products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
                ]
-best_buy = Store(product_list)
+
+best_buy = store.Store(product_list)
 
 def show_all_products(store):
     print("All products in store:")
--- products.py
+++ products.py
@@ -37,7 +37,48 @@ class Product():
             raise ValueError("Not enough quantity in stock")
         
         self.quantity -= quantity
-        
+
         if self.quantity == 0:
             self.deactivate()
         return quantity * self.price
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        if not isinstance(name, str) or not name:
+            raise ValueError("Name must be a non-empty string")
+        if not isinstance(price, (int, float)) or price <= 0:
+            raise ValueError("Price must be a positive number")
+        
+        self.name = name
+        self.price = price 
+        self.active = False
+    def buy(self, quantity):
+        if not isinstance(quantity, (int, float)) or quantity <= 0:
+            raise ValueError("Quantity must be a positive number")
+        
+    def show(self):
+        return f"{self.name})"        
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity,  maximum):
+        if not isinstance(name, str) or not name:
+            raise ValueError("Name must be a non-empty string")
+        if not isinstance(price, (int, float)) or price <= 0:
+            raise ValueError("Price must be a positive number")
+        
+        self.name = name
+        self.price = price 
+        self.active = False
+        self.maximum = maximum
+        
+    def buy(self, quantity):
+        if not isinstance(quantity, (int, float)) or quantity <= 0:
+            raise ValueError("Quantity must be a positive number")
+        
+        if quantity > self.maximum:
+            raise ValueError("Not enough quantity in stock")
+        
+    def show(self):
+        return f"{self.name}, Max quantity: {self.maximum})"        
+      
+
